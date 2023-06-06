883e3c135558656cb2fa8a2a03b6b9feebd0893c
Oleg Danko <olegdanko@outlook.com>
2023-06-06 03:55:11 +0300

    Added test cases for AttributeStateFrame::get_attr and AttributeStateFrame::spawn

---
+++ include/Attributes.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <unordered_map>
+#include <memory>
+
+
+struct AttributePosition {
+    float x, y, z;
+};
+
+template<typename ATTR_T>
+struct AttributeGeneratorDefault {
+    using attr_holder_t = std::unique_ptr<ATTR_T>;
+    using attr_ptr_t = ATTR_T*;
+    static attr_holder_t gen_attribure() {
+        return std::make_unique<ATTR_T>();
+    }
+    static attr_holder_t gen_attribure(attr_ptr_t attr) {
+        auto attr_ret = std::make_unique<ATTR_T>();
+        *attr_ret = *attr;
+        return attr_ret;
+    }
+};
+
+template<typename ATTR_GEN_T>
+struct AttributeStateFrame {
+    using attr_holder_t = typename ATTR_GEN_T::attr_holder_t;
+    using attr_ptr_t = typename ATTR_GEN_T::attr_ptr_t;
+
+    static AttributeStateFrame spawn(AttributeStateFrame& src) {
+        AttributeStateFrame frame;
+
+        auto& src_map = src.attr_holder_map;
+        auto& dst_map = frame.attr_holder_map;
+        for(auto& [id, attr] : src_map) {
+            dst_map[id] = ATTR_GEN_T::gen_attribure(attr.get());
+        }
+
+        return frame;
+    }
+
+    // holds attributes created or updated during the current frame
+    std::unordered_map<size_t, attr_holder_t> attr_holder_map;
+
+    attr_ptr_t get_attr(size_t id) {
+        if(attr_holder_map.find(id) == attr_holder_map.end()) {
+            attr_holder_map[id] = ATTR_GEN_T::gen_attribure();
+        }
+        return attr_holder_map[id].get();
+    }
+};
+
+template<typename ATTR_T>
+using AttributeStateFrame_DefGen = AttributeStateFrame<AttributeGeneratorDefault<ATTR_T>>;
---
+++ tests/TestAttributeStateFrame.cpp
@@ -0,0 +1,37 @@
+#include <boost/test/unit_test.hpp>
+#include <Attributes.hpp>
+
+BOOST_AUTO_TEST_SUITE(AttributeStateFrameTests)
+
+BOOST_AUTO_TEST_CASE(AttributeStateFrame_get_attr) {
+    struct TestAttr { int var{0}; };
+    AttributeStateFrame_DefGen<TestAttr> frame;
+
+    // frame.get_attr() always returns a value
+    auto attr_ptr = frame.get_attr(1);
+    BOOST_CHECK(attr_ptr != nullptr);
+
+    // frame.get_attr() returns a pointer to the same variable each time
+    attr_ptr->var = 1;
+    BOOST_CHECK_EQUAL(frame.get_attr(1)->var, 1);
+}
+
+BOOST_AUTO_TEST_CASE(AttributeStateFrame_spawn) {
+    struct TestAttr { int var{0}; };
+    using frame_t = AttributeStateFrame_DefGen<TestAttr>;
+
+    frame_t frame_0;
+    frame_0.get_attr(1)->var = 1;
+
+    auto frame_1 = frame_t::spawn(frame_0);
+
+    auto attr_ptr = frame_1.get_attr(1);
+    BOOST_CHECK(attr_ptr != nullptr);
+    BOOST_CHECK_EQUAL(attr_ptr->var, 1);
+
+    // Non-functional requirement - frame_1.get_attr() returns a value distinct from frame_0.get_attr()
+    frame_0.get_attr(1)->var = 2;
+    BOOST_CHECK_EQUAL(frame_1.get_attr(1)->var, 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
