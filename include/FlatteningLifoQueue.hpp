d94ecc10cc979f02662168e2d28330c62edb056f
Oleg Danko <olegdanko@outlook.com>
2023-06-06 05:51:20 +0300

    Created FlatteningLifoQueue

---
+++ include/FlatteningLifoQueue.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+template<typename T>
+void flatten(T& above, T& below);
+
+template<typename T>
+struct FlatteningLifoQueue {
+    void push(T val) {
+
+    }
+    void observe_front() {
+
+    }
+};
---
+++ tests/TestFlatteningLifoQueue.cpp
@@ -0,0 +1,10 @@
+#include <boost/test/unit_test.hpp>
+#include <FlatteningLifoQueue.hpp>
+
+BOOST_AUTO_TEST_SUITE(FlatteningLifoQueueTests)
+
+BOOST_AUTO_TEST_CASE(FlatteningLifoQueue_push) {
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
