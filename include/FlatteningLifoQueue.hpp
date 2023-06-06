c77ae26191f93463a6676cb9c83c8d97ac731525
Oleg Danko <olegdanko@outlook.com>
2023-06-06 18:14:42 +0300

    Implemented FlatteningLifoQueue for several items

--- include/FlatteningLifoQueue.hpp
+++ include/FlatteningLifoQueue.hpp
@@ -1,14 +1,80 @@
 #pragma once
+#include <memory>
 
 template<typename T>
-void flatten(T& above, T& below);
+struct flattener;
 
 template<typename T>
 struct FlatteningLifoQueue {
-    void push(T val) {
+    struct Node {
+        Node *next {nullptr};
+        std::unique_ptr<Node> prev{nullptr};
+        size_t observers{0};
+        T val;
 
+        static void exclude(Node* node) {
+            auto& prev = node->prev;
+            auto next = node->next;
+            if(prev)
+                prev->next = next;
+            if(next)
+                next->prev = std::move(prev);
+        }
+
+        static void flatten_chain(Node* node) {
+            if(node->observers > 0)
+                return;
+
+            while(node->prev && node->prev->observers == 0) {
+                node = node->prev.get();
+            }
+
+            flattener<T> f;
+            while(node->next && node->next->observers == 0) {
+                auto next = node->next;
+                f(next->val, node->val);
+                exclude(node);
+                node = next;
+            }
+        }
+    };
+    std::unique_ptr<Node> top{nullptr};
+
+    struct Observer {
+        Node* node;
+        T& get() {
+            return node->val;
+        }
+        Observer(Node* n) : node(n) {
+            ++(node->observers);
+        }
+        Observer(Observer&& that) : node(that.node) {
+            that.node = nullptr;
+        }
+        ~Observer() {
+            if(!node)
+                return;
+            --(node->observers);
+            Node::flatten_chain(node);
+        }
+    };
+
+    void push(T v) {
+        auto node = std::make_unique<Node>();
+
+        if(top) {
+            top->next = node.get();
+        }
+
+        node->prev = std::move(top);
+        node->val = std::move(v);
+
+        top = std::move(node);
+
+        Node::flatten_chain(top.get());
     }
-    void observe_front() {
-
+    Observer observe() {
+        return {top.get()};
     }
 };
+
--- tests/TestFlatteningLifoQueue.cpp
+++ tests/TestFlatteningLifoQueue.cpp
@@ -1,10 +1,101 @@
 #include <boost/test/unit_test.hpp>
 #include <FlatteningLifoQueue.hpp>
+#include <optional>
+
+
+struct TestInt { int val; };
+
+template<>
+struct flattener<TestInt> {
+    void operator()(TestInt& top_saved, TestInt& bottom_dismissed) const {
+        top_saved.val += bottom_dismissed.val;
+    }
+};
+
 
 BOOST_AUTO_TEST_SUITE(FlatteningLifoQueueTests)
 
 BOOST_AUTO_TEST_CASE(FlatteningLifoQueue_push) {
-
+    std::hash<std::string> h;
+    FlatteningLifoQueue<TestInt> queue;
+    queue.push({1});
+    BOOST_CHECK_EQUAL(queue.observe().get().val, 1);
 }
+
+BOOST_AUTO_TEST_CASE(FlatteningLifoQueue_push_several) {
+    std::hash<std::string> h;
+    FlatteningLifoQueue<TestInt> queue;
+    queue.push({1});
+    queue.push({3});
+    BOOST_CHECK_EQUAL(queue.observe().get().val, 4);
+}
+
+BOOST_AUTO_TEST_CASE(FlatteningLifoQueue_push_observe_two) {
+    std::hash<std::string> h;
+    FlatteningLifoQueue<TestInt> queue;
+    queue.push({1});
+    if(auto obs_0 = queue.observe(); true) {
+        queue.push({3});
+        BOOST_CHECK_EQUAL(queue.observe().get().val, 3);
+    }
+    BOOST_CHECK_EQUAL(queue.observe().get().val, 4);
+}
+
+BOOST_AUTO_TEST_CASE(FlatteningLifoQueue_push_observe_three) {
+    std::hash<std::string> h;
+    FlatteningLifoQueue<TestInt> queue;
+    queue.push({1});
+    queue.push({3});
+    queue.push({5});
+
+    BOOST_CHECK_EQUAL(queue.observe().get().val, 9);
+}
+
+BOOST_AUTO_TEST_CASE(FlatteningLifoQueue_push_observe_2_of_3) {
+    std::hash<std::string> h;
+    FlatteningLifoQueue<TestInt> queue;
+    queue.push({1});
+    queue.push({3});
+    auto obs_opt = std::make_optional(queue.observe());
+    queue.push({5});
+
+    BOOST_CHECK_EQUAL(queue.observe().get().val, 5);
+    obs_opt = std::nullopt;
+    BOOST_CHECK_EQUAL(queue.observe().get().val, 9);
+}
+
+BOOST_AUTO_TEST_CASE(FlatteningLifoQueue_push_2_of_3_last) {
+    std::hash<std::string> h;
+    FlatteningLifoQueue<TestInt> queue;
+    queue.push({1});
+    auto obs_0_opt = std::make_optional(queue.observe());
+    queue.push({3});
+    auto obs_1_opt = std::make_optional(queue.observe());
+    queue.push({5});
+
+    BOOST_CHECK_EQUAL(queue.observe().get().val, 5);
+
+    obs_0_opt = std::nullopt;
+    obs_1_opt = std::nullopt;
+    BOOST_CHECK_EQUAL(queue.observe().get().val, 9);
+}
+
+BOOST_AUTO_TEST_CASE(FlatteningLifoQueue_push_1_of_3_last) {
+    std::hash<std::string> h;
+    FlatteningLifoQueue<TestInt> queue;
+    queue.push({1});
+    auto obs_0_opt = std::make_optional(queue.observe());
+    queue.push({3});
+    auto obs_1_opt = std::make_optional(queue.observe());
+    queue.push({5});
+
+    BOOST_CHECK_EQUAL(queue.observe().get().val, 5);
+
+    obs_1_opt = std::nullopt;
+    BOOST_CHECK_EQUAL(queue.observe().get().val, 8);
+    obs_0_opt = std::nullopt;
+    BOOST_CHECK_EQUAL(queue.observe().get().val, 9);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
